f75fb36afa2545d63b4148ea6789fc1a9ee3f12b
Kosuke Miyasaka <komiyasa@microsoft.com>
2020-08-11 12:09:59 +0900

    First Commit

---
+++ Data/MvcBasicContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcBasic.Models;
+
+namespace MvcBasic.Data
+{
+    public class MvcBasicContext : DbContext
+    {
+        public MvcBasicContext (DbContextOptions<MvcBasicContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcBasic.Models.Member> Member { get; set; }
+    }
+}
---
+++ Migrations/20200811030336_IntilModels.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcBasic.Migrations
+{
+    public partial class IntilModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Member",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Birth = table.Column<DateTime>(nullable: false),
+                    Married = table.Column<bool>(nullable: false),
+                    Memo = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Member", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Member");
+        }
+    }
+}
