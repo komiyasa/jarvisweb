8452c91d776ec5eeb947b420c76f3d0ceaf4e76b
Kosuke Miyasaka <komiyasa@microsoft.com>
2020-08-11 13:05:20 +0900

    画像をアップロードするサービス追加

--- Models/Member.cs
+++ Models/Member.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace MvcBasic.Models
 {
     public class Member
     {
         public int Id { get; set; }
+
+        [DisplayName("氏名")]
         public string Name { get; set; }
         public string Email { get; set; }
         public DateTime Birth { get; set; }
